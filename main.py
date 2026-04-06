6f863216fb369aa8e9671226a67f9208a64799e9
myoungjin <mjgo@babitalk.com>
2026-04-06 09:15:40 +0900

    ref: main.py에서 불필요한 hello2, hello3 함수 제거
    - hello2와 hello3 함수를 삭제하여 코드 간결성 향상
    - main 함수에서 해당 함수 호출 제거

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 def hello():
     print("hello world")
 
-def hello2():
-    print("hello korea")
-
-def hello3():
-    print("hello python")
-
 def main():
     hello()
-    hello2()
-    hello3()
 
 if __name__ == "__main__":
     main()
